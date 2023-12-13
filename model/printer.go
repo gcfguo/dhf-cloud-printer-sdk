2720650b62ba656143589eeb350d8a6b476ff84a
ruohuaii <bfdbfsnl@github.com>
2023-12-13 13:36:10 +0800

    refactor:优化打印机列表数据结构

--- model/printer.go
+++ model/printer.go
@@ -26,14 +26,16 @@ type (
 		PageSize int `json:"page_size"`
 	}
 	ListPrinterRes struct {
-		List []struct {
-			PrinterName string `json:"printer_name" `
-			HostID      string `json:"host_id"     `
-			PrinterId   string `json:"printer_id"   `
-			Status      string `json:"status"       `
-			CreateTime  string `json:"create_time"  `
-		} `json:"list"`
-		Total int `json:"total"`
+		List  []*PrinterInf `json:"list"`
+		Total int           `json:"total"`
+	}
+	PrinterInf struct {
+		PrinterName string `json:"printer_name" `
+		HostID      string `json:"host_id"     `
+		PrinterId   string `json:"printer_id"   `
+		PrinterType string `json:"printer_type"`
+		Status      string `json:"status"       `
+		CreateTime  string `json:"create_time"  `
 	}
 )
 
